35bb5fe2d39cacaf3b00b6fd5fb6a9778759b240
Md Abubakar <58105090+MdAbubakar@users.noreply.github.com>
2023-03-07 23:41:14 +0530

    Image Component created

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,8 +1,22 @@
 import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useProductContext } from "../context/productcontext";
+import PageNavigation from "../components/PageNavigation";
+import MyImage from "../components/MyImage";
+import { Container } from "../styles/Container";
+import FormatedPrice from "../helpers/FormatedPrice";
+import { TbTruckDelivery, TbReplace } from "react-icons/tb";
+import { MdSecurity } from "react-icons/md";
 
+const API = "https://api.pujakaitem.com/api/products";
 const Wrapper = styled.section`
   .container {
     padding: 9rem 0;
+  }
+  .product_images {
+    display: flex;
+    align-items: center;
   }
   .product-data {
     display: flex;
@@ -10,7 +24,6 @@ const Wrapper = styled.section`
     align-items: flex-start;
     justify-content: center;
     gap: 2rem;
-
     .product-data-warranty {
       width: 100%;
       display: flex;
@@ -18,12 +31,11 @@ const Wrapper = styled.section`
       align-items: center;
       border-bottom: 1px solid #ccc;
       margin-bottom: 1rem;
-
       .product-warranty-data {
         text-align: center;
-
         .warranty-icon {
           background-color: rgba(220, 220, 220, 0.5);
+          color: ${({ theme }) => theme.colors.btn};
           border-radius: 50%;
           width: 4rem;
           height: 4rem;
@@ -35,7 +47,6 @@ const Wrapper = styled.section`
         }
       }
     }
-
     .product-data-price {
       font-weight: bold;
     }
@@ -47,12 +58,10 @@ const Wrapper = styled.section`
       flex-direction: column;
       gap: 1rem;
       font-size: 1.8rem;
-
       span {
         font-weight: bold;
       }
     }
-
     hr {
       max-width: 100%;
       width: 90%;
@@ -61,20 +70,111 @@ const Wrapper = styled.section`
       color: red;
     }
   }
-
   .product-images {
     display: flex;
     justify-content: center;
     align-items: center;
   }
-
+  .page_loading {
+    font-size: 3.2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
   @media (max-width: ${({ theme }) => theme.media.mobile}) {
     padding: 0 2.4rem;
   }
 `;
 
 const Singleproduct = () => {
-  return <Wrapper></Wrapper>;
+  const { getSingleProduct, isSingleLoading, singleProduct } =
+    useProductContext();
+
+  const { id } = useParams();
+
+  const {
+    name,
+    company,
+    price,
+    description,
+    image,
+    category,
+    stock,
+    stars,
+    reviews,
+  } = singleProduct;
+
+  useEffect(() => {
+    getSingleProduct(`${API}?id=${id}`);
+  }, []);
+
+  if (isSingleLoading) {
+    return <div className="page_loading">Loading.....</div>;
+  }
+
+  return (
+    <Wrapper>
+      <PageNavigation title={name} />
+      <Container className="container">
+        <div className="grid grid-two-column">
+          {/* product Images  */}
+          <div className="product_images">
+            <MyImage imgs={image} />
+          </div>
+
+          {/* product dAta  */}
+          <div className="product-data">
+            <h2>{name}</h2>
+            <p>{stars}</p>
+            <p>{reviews} reviews</p>
+            <p className="product-data-price">
+              MRP:
+              <del>
+                <FormatedPrice price={price + 250000} />
+              </del>
+            </p>
+            <p className="product-data-price product-data-real-price">
+              Deal of the Day: <FormatedPrice price={price} />
+            </p>
+            <p>{description}</p>
+            <div className="product-data-warranty">
+              <div className="product-warranty-data">
+                <TbTruckDelivery className="warranty-icon" />
+                <p>Free Delivery</p>
+              </div>
+              <div className="product-warranty-data">
+                <TbReplace className="warranty-icon" />
+                <p>30 Days Replacement</p>
+              </div>
+              <div className="product-warranty-data">
+                <TbTruckDelivery className="warranty-icon" />
+                <p>E-Com Express</p>
+              </div>
+              <div className="product-warranty-data">
+                <MdSecurity className="warranty-icon" />
+                <p>2 Year's Warranty</p>
+              </div>
+            </div>
+
+            <div className="product-data-info">
+              <p>
+                Available:
+                <span>{stock > 0 ? " In Stock" : " Out of Stock"}</span>
+              </p>
+              <p>
+                Category:
+                <span> {category}</span>
+              </p>
+              <p>
+                Brand:
+                <span> {company}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Singleproduct;
